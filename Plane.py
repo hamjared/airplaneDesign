ecdc0f7d4759b8c2c3efcc7dd4d75aef2cc35a94
hamjared <hamjared5@gmail.com>
2018-05-30 07:05:09 -0400

    moved calculation of top view points inside of plane class

--- Plane.py
+++ Plane.py
@@ -2,6 +2,7 @@ from Fuselage import Fuselage
 from Wing import Wing
 from Empennage import Empennage
 from Component import Component
+import operatingConditions as opCond
 
 class Plane:
 
@@ -10,6 +11,8 @@ class Plane:
         self.wing = wing
         self.empennage = empennage
         self.components = components
+        self.cg = -1
+        self.calcCG()
 
     def calcCG(self):
         xW = 0
@@ -17,7 +20,71 @@ class Plane:
         for component in self.components:
             xW += component.weight * component.x_location
             W += component.weight
+        self.cg = xW/W
         return xW/W
+
+    def wingTopViewPoints(self):
+        x1 = -1 * self.fuselage.fuselageWidth / 2
+        y1 = self.cg - 0.3 * self.wing.MAC
+
+        x2 = x1 + -1 * self.wing.wingSpan / 2
+        y2 = y1 + (self.wing.rootChord - self.wing.tipChord)
+
+        x3 = x2
+        y3 = y2 + self.wing.tipChord
+
+        x4 = x1
+        y4 = y3
+
+        x5 = -x1
+        y5 = y1
+
+        x6 = x5 + self.wing.wingSpan / 2
+        y6 = y2
+
+        x7 = x6
+        y7 = y3
+
+        x8 = x5
+        y8 = y7
+
+        X_Right_wing = [x1, x2, x3, x4]
+        Y_Right_wing = [-y1, -y2, -y3, -y4]
+
+        X_Left_wing = [x5, x6, x7, x8]
+        Y_Left_wing = [-y5, -y6, -y7, -y8]
+
+        return X_Right_wing, Y_Right_wing, X_Left_wing, Y_Left_wing
+
+    def empennageTopViewPoints(self):
+        x0 = 0
+        y0 = opCond.l_ht + self.cg - 0.25 * self.empennage.ht.MAC
+
+        x1 = -1 * self.fuselage.thrustTubeOutletDiameter / 2
+        y1 = opCond.l_ht + self.cg - 0.25 * self.empennage.ht.MAC
+
+        x2 = x1 + -1 * self.empennage.ht.wingSpan / 2
+        y2 = y1 + (self.empennage.ht.rootChord - self.empennage.ht.tipChord)
+
+        x3 = x2
+        y3 = y2 + self.empennage.ht.tipChord
+
+        x4 = -x2
+        y4 = y3
+
+        x5 = -x2
+        y5 = y2
+
+        x6 = -x1
+        y6 = y1
+
+        x7 = 0
+        y7 = y6
+
+        X_ht = [x0, x1, x2, x3, x4, x5, x6, x7]
+        Y_ht = [-y0, -y1, -y2, -y3, -y4, -y5, -y6, -y7]
+
+        return X_ht, Y_ht
 
 
 
--- main.py
+++ main.py
@@ -39,79 +39,27 @@ print(plane.calcCG())
 cg = plane.calcCG()
 
 
-x1 = -1* fuselage.fuselageWidth/2
-y1 = cg -  0.25*wing.MAC
+X_Right_wing, Y_Right_wing, X_Left_wing, Y_Left_wing = plane.wingTopViewPoints()
 
-x2 = x1 + -1 * wing.wingSpan/2
-y2 = y1 + (wing.rootChord-wing.tipChord)
-
-x3 = x2
-y3 = y2 + wing.tipChord
-
-x4 = x1
-y4 = y3
-
-x5 = -x1
-y5 = y1
-
-x6 = x5 + wing.wingSpan/2
-y6 = y2
-
-x7 = x6
-y7 = y3
-
-x8 = x5
-y8 = y7
-
-X_Right_wing = [x1, x2, x3, x4]
-Y_Right_wing = [-y1, -y2, -y3, -y4]
-
-X_Left_wing = [ x5, x6, x7, x8]
-Y_Left_wing = [-y5, -y6, -y7, -y8]
-
-x0 = 0
-y0 = opCond.l_ht + cg - 0.25 * empennage.ht.MAC
-
-x1 = -1* fuselage.thrustTubeOutletDiameter/2
-y1 = opCond.l_ht + cg - 0.25 * empennage.ht.MAC
-
-x2 = x1 + -1 * empennage.ht.wingSpan/2
-y2 = y1 + (empennage.ht.rootChord- empennage.ht.tipChord)
-
-x3 = x2
-y3 = y2 + empennage.ht.tipChord
-
-x4 = -x2
-y4 = y3
-
-x5 = -x2
-y5 = y2
-
-x6 = -x1
-y6 = y1
-
-x7 = 0
-y7 = y6
-
-X_ht = [x0, x1, x2, x3, x4, x5, x6, x7]
-Y_ht = [-y0, -y1, -y2, -y3, -y4, -y5, -y6, -y7]
+X_ht, Y_ht = plane.empennageTopViewPoints()
 
 X_fuse, Y_fuse = fuselage.topViewPoints()
+
 plt = matplotlib.pyplot
 plt.figure(1)
-plt.plot(X_Right_wing, Y_Right_wing, X_Left_wing, Y_Left_wing, X_ht, Y_ht, X_fuse, Y_fuse, [fuselage.fuselageWidth/2,-fuselage.fuselageWidth/2], [-cg, -cg])
+plt.plot(X_Right_wing, Y_Right_wing, X_Left_wing, Y_Left_wing, X_ht, Y_ht, X_fuse, Y_fuse, [fuselage.fuselageWidth/2, -fuselage.fuselageWidth/2], [-cg, -cg])
 plt.axis('equal')
 
-plt.figure(2)
-alpha = [x for x in range(-5, 10)]
-C_l = wing.calcC_lRange(-5,10)
-plt.plot(alpha, C_l)
-plt.grid()
-
-plt.figure(3)
-C_d = wing.calcC_dRange(-5, 10)
-plt.plot(alpha, C_d)
-plt.grid()
+# plt.figure(2)
+# alpha = [x for x in range(-5, 10)]
+# C_l = wing.calcC_lRange(-5,10)
+# plt.plot(alpha, C_l)
+# plt.grid()
+#
+# plt.figure(3)
+# C_d = wing.calcC_dRange(-5, 10)
+# plt.plot(alpha, C_d)
+# plt.grid()
 
 plt.show()
 
